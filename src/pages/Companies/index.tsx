906783298f61e1b3068a2dd47b2a37960cf1504f
Omar Mohamed <omer.eldanin@gmail.com>
2026-02-05 15:47:15 +0200

    edit

--- src/pages/Companies/index.tsx
+++ src/pages/Companies/index.tsx
@@ -139,7 +139,7 @@ export default function Companies() {
                       <td className="p-3 border-b border-gray-200">
                         <div className="flex gap-2">
                           <button
-                            className="bg-gradient-to-r from-[#1c46a2] to-[#31e5b7] text-white px-4 py-2 rounded-lg hover:bg-gray-800"
+                            className="bg-gray-800 text-white px-4 py-2 rounded-lg hover:bg-gray-800"
                             onClick={() => navigation("/companies/filters")}>
                             الفلاتر
                           </button>
