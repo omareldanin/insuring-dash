5507edbafe41d23278e328828600e2fcd1d52edb
Omar Mohamed <omer.eldanin@gmail.com>
2026-03-12 15:07:19 +0200

    edit

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 // import toast from "react-hot-toast";
 
-// export const baseURL = "https://api.bfibrokerage.com/";
-export const baseURL = "http://localhost:3000/";
+export const baseURL = "https://api.bfibrokerage.com/";
+// export const baseURL = "http://localhost:3000/";
 
 export interface APIError {
   message: string;
