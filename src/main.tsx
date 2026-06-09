f51086ded17dec08197eae23ad7da6b76af89fd5
Omar Mohamed <omer.eldanin@gmail.com>
2026-06-09 09:37:24 +0300

    edit

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import { createTheme, ThemeProvider } from "@mui/material";
+import { MantineProvider } from "@mantine/core";
 
 export const queryClient = new QueryClient({
   defaultOptions: {
@@ -17,9 +18,12 @@ const theme = createTheme({
 });
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <MantineProvider>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </MantineProvider>
+
     <Toaster position="top-center" />
   </QueryClientProvider>,
 );
